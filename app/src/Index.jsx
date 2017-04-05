62b6de66cb7227ba4cc0b014499f4c95e854da05
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-05 11:51:08 -0700

    move filepaths

--- app/src/Index.jsx
+++ app/src/Index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
 import './main.css';
 
 class App extends React.Component {
@@ -17,4 +22,16 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  document.getElementById('app'),
+);
