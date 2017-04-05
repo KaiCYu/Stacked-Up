a56bbce93b61f9a5362a827a1b69a5855103eccf
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-05 11:34:22 -0700

    add store to Index.jsx and set up redux devtools

---
+++ app/Index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import './main.css';
+import App from './components/App';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
