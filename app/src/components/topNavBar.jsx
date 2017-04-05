201bb253b4c6a143dc7e46d983a104aa351fd0a7
billywon <billy.y.won@gmail.com>
2017-04-05 11:31:39 -0700

    (feat) implement TopNavBar

---
+++ app/src/components/topNavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopNavBar = () => (
+  <div>
+    <span>StackedUp</span>
+    <input type="text" placeholder="search" />
+    <button>Sign In</button>
+    <button>Sign Up</button>
+  </div>
+);
+
+export default TopNavBar;
