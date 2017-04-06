295aa5627a7e092b8e1b366abaa1d88cf838f91f
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-05 20:06:25 -0700

    (feat) create database schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE StackedUp;
 
 USE StackedUp;
 
-CREATE TABLE applicants (
+CREATE TABLE applicants(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR(255),
   password VARCHAR(255),
   phone_number INT(20),
   email VARCHAR(255),
@@ -13,22 +13,50 @@ CREATE TABLE applicants (
   UNIQUE INDEX(username)
 );
 
-CREATE TABLE job_postings (
-
+CREATE TABLE employer(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255),
+  password VARCHAR(255),
+  phone_number INT,
+  email VARCHAR(255),
+  logo_url VARCHAR(255), -- will be hosted on a cloud service
+  job_postings_id INT
+  -- FOREIGN KEY (job_postings_id) REFERENCES job_postings(id)
 );
 
-CREATE TABLE employer (
-
+CREATE TABLE job_postings(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  position VARCHAR(255),
+  description VARCHAR(255),
+  location VARCHAR(255),
+  salary INT,
+  post_date DATE,
+  employer_id INT,
+  FOREIGN KEY (employer_id) REFERENCES employer(id)
 );
 
-CREATE TABLE skills (
-
+CREATE TABLE skills(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  skill VARCHAR(255)
 );
 
-CREATE TABLE applicants_skills (
-
+CREATE TABLE applicants_skills(
+  skill_id INT,
+  applicant_id INT,
+  FOREIGN KEY (skill_id) REFERENCES skills(id),
+  FOREIGN KEY (applicant_id) REFERENCES applicants(id)
 );
 
-CREATE TABLE applicants_job_postings (
-
+CREATE TABLE applicants_job_postings(
+  applicant_id INT,
+  job_posting_id INT,
+  FOREIGN KEY (applicant_id) REFERENCES applicants(id),
+  FOREIGN KEY (job_posting_id) REFERENCES job_postings(id)
 );
+
+-- Alter employer table to have a foreign key that references job_postings table.
+-- Required because employer won't be able to reference a table that hasn't been created yet.
+-- Problem exists because of the order these tables must be created.
+ALTER TABLE employer
+  ADD CONSTRAINT FK_job_postings_employer FOREIGN KEY (job_postings_id) REFERENCES employer(id)
+
