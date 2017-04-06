add71cbcb78277de694c865b8518a20208bbad57
billywon <billy.y.won@gmail.com>
2017-04-05 21:03:01 -0700

    (fix) get rid of signup client duplication

--- app/src/components/SignupClient.jsx
+++ app/src/components/SignupClient.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const SignupClient = (props) => (
-  <div className='SignupClient-container'>
+  <div className="SignupClient-container">
     <h1>SignupClient</h1>
     <div>
-    	SignUp a Client here
+      <p> welcome! </p>
+      <span> Name: </span><input type="text" /><br />
+      <span> Address: </span><input type="text" /><br />
+      <span> Phone Number: </span><input type="text" /><br />
+      <span> upload your resume </span><br />
+      <span> upload your cover letter </span><br />
+      <span> upload your picture </span><br />
+      <button>Sign up</button>
+      <button>Cancel</button>
     </div>
   </div>
-)
+);
 
 export default SignupClient;
