776ba201ea5e5bb45bd337fbda5f870edd658497
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-06 16:07:03 -0700

    (feat) add automatically creating a new mysql database when server restarts

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,25 @@
 const mysql = require('mysql');
+const schema = require('./schema.js');
+const Promise = require('bluebird');
+
+const database = 'stackedup';
 
 // Create a database connection and export it from this file.
 exports.connection = mysql.createConnection({
   user: 'root',
   password: '',
-  database: 'StackedUp',
 });
 
-exports.connection.connect();
+const db = Promise.promisifyAll(exports.connection, { multiArgs: true });
+
+db.connectAsync()
+  .then(() => db.queryAsync(`DROP DATABASE IF EXISTS ${database}`))
+  .then(() => db.queryAsync(`CREATE DATABASE ${database}`))
+  .then(() => db.queryAsync(`USE ${database}`))
+  .then(() => db.queryAsync(schema.applicants))
+  .then(() => db.queryAsync(schema.employer))
+  .then(() => db.queryAsync(schema.job_postings))
+  .then(() => db.queryAsync(schema.skills))
+  .then(() => db.queryAsync(schema.applicants_skills))
+  .then(() => db.queryAsync(schema.applicants_job_postings))
+  .then(() => db.queryAsync(schema.alter_employer));
