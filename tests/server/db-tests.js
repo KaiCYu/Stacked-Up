b49a63483df81c07ce3b609039d5d102dbfc821e
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-06 17:07:17 -0700

    init test suite with mocha/chai

---
+++ tests/server/db-tests.js
@@ -0,0 +1,12 @@
+// test file for database
+/* global define, it, describe */
+const chai = require('chai');
+const Db = require('./../../server/db/index.js');
+
+const expect = chai.expect;
+
+describe('Db', () => {
+  it('should create a stackedup database', () => {
+
+  });
+});
