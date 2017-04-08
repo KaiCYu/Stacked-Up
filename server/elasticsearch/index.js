b10332f23b4f985b14a27acf3aa734ea54bcca54
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-08 16:12:48 -0700

    (feat) add elasticsearch helper functions

--- server/elasticsearch/index.js
+++ server/elasticsearch/index.js
@@ -1,19 +1,24 @@
 // index.js - elasticsearch
+// main() is at the bottom of the file
 const elasticsearch = require('elasticsearch');
+const Promise = require('bluebird');
+let db = require('./../db/index.js').connection;
 const fs = require('fs');
 
+db = Promise.promisifyAll(db, { multiArgs: true });
+
 // create elasticsearch connection
-exports.esClient = new elasticsearch.Client({
+const esClient = new elasticsearch.Client({
   host: '127.0.0.1:9200',
   log: 'error',
 });
 
-// index JSON data
-exports.bulkIndex = function bulkIndex(index, type, data) {
-  const bulkBody = [];
 
-  data.forEach(item => {
-    bulkBody.push({
+// index json data
+const bulkindex = (index, type, data) => {
+  const bulkbody = [];
+  data.forEach((item) => {
+    bulkbody.push({
       index: {
         _index: index,
         _type: type,
@@ -21,67 +26,77 @@ exports.bulkIndex = function bulkIndex(index, type, data) {
       },
     });
 
-    bulkBody.push(item);
+    bulkbody.push(item);
   });
 
-  esClient.bulk({ body: bulkBody })
+  esClient.bulk({ body: bulkbody })
   .then((response) => {
     console.log('here');
-    let errorCount = 0;
+    let errorcount = 0;
     response.items.forEach((item) => {
       if (item.index && item.index.error) {
-        console.log(++errorCount, item.index.error);
+        console.log('error from index', ++errorcount, item.index.error);
       }
     });
     console.log(
-      `Successfully indexed ${data.length - errorCount}
+      `successfully indexed ${data.length - errorcount}
        out of ${data.length} items`
     );
   })
-  .catch(console.err);
+  .catch((error) => {
+    console.error('error from index', error);
+  });
 };
 
-// index data from ./data.json
-exports.indexData = function test() {
-  const articles = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-  bulkIndex('library', 'article', articles);
+// query database for data
+const queryDatabase = () => {
+  return db.queryAsync('select * from applicants');
 };
 
-// check indices
-exports.indices = () => {
+// check indices of elasticsearch
+const indices = () => {
   return esClient.cat.indices({ v: true })
   .then(console.log)
-  .catch(err => console.error(`Error connecting to the es client: ${err}`));
+  .catch(err => console.error(`error connecting to the es client: ${err}`));
 };
 
 // search index
-exports.search = function search(index, body) {
+const search = (index, body) => {
   return esClient.search({ index, body });
 };
 
-const test = function test() {
+// example function
+const test = () => {
   const body = {
-    size: 20,
+    size: 1,
     from: 0,
     query: {
       match: {
-        title: {
-          query: 'lorem ipsum dolor sit', // query string
-          minimum_should_match: 3, // minimum number of words that should match in query
+        email: {
+          query: 'bwon', // query string
+          minimum_should_match: 0, // minimum number of words that should match in query
           fuzziness: 0, // number of closely spelled terms
         },
       },
     },
   };
 
-  search('library', body)
+  search('stackedup', body)
   .then((results) => {
     console.log(`found ${results.hits.total} items in ${results.took}ms`);
     console.log('returned article titles:');
-    results.hits.hits.forEach((hit, index) => console.log(
-        `\t${body.from + ++index} - ${hit._source.title}`
-      )
-    )
+    results.hits.hits.forEach(item => console.log(item));
   })
   .catch(console.error);
 };
+
+const main = (() => {
+  // queryDatabase()
+  // .then((queryResults) => {
+  //   const data = queryResults[0];
+  //   bulkindex('stackedup', 'applicants', data);
+  // });
+  test();
+})();
+
+module.exports = exports = main;
