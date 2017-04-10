bb00a1ea2f86ca7850c24604e9bef04471740ba8
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-10 15:15:18 -0700

    add test for elasticsearch

---
+++ tests/client/search-tests.js
@@ -0,0 +1,55 @@
+// test file for search endpoint
+/* global define, it, describe, beforeEach, afterEach */
+const Promise = require('bluebird');
+const chai = require('chai');
+const express = require('express');
+const mysql = require('mysql');
+const request = require('request');
+const esConfig = require('./../../server/elasticsearch/index.js');
+
+const expect = chai.expect;
+
+describe('Search Applicants', () => {
+  const database = 'stackedup';
+  let db;
+
+  beforeEach((done) => {
+    const connection = mysql.createConnection({
+      user: 'root',
+      password: '',
+      database,
+    });
+
+    db = Promise.promisifyAll(connection, { multiArgs: true });
+    db.connectAsync()
+    .then(() => db.queryAsync('DELETE FROM applicants', done));
+  });
+
+  afterEach(() => {
+    db.end();
+  });
+
+  it('should match the username if searched exactly', (done) => {
+    db.queryAsync('INSERT INTO applicants (username, fullname, password, phone_number, email, resume_url, photo_url)' +
+      'VALUES ("gabece", "gabriel certeza", "password", 1947193442, "gabrielscerteza@gmail.com", "example.com", "example2.com")'
+    )
+    .then(() => db.queryAsync('SELECT * FROM applicants WHERE username="gabece"'))
+    .then((result) => {
+      const user = result[0];
+
+      // index for elasticsearch
+      esConfig.bulkIndex('stackedup', 'applicants', user);
+      request.get('http://localhost:8000/search/applicants/username/gabece/1', (error, result) => {
+        const body = JSON.parse(result.body);
+        const user = body[0];
+        expect(body).to.not.equal(null);
+        expect(body.length).to.equal(1);
+        expect(user.username).to.equal('gabece');
+        expect(user.fullname).to.equal('gabriel certeza');
+        expect(user.password).to.equal('password');
+        expect(user.email).to.equal('gabrielscerteza@gmail.com');
+        done();
+      });
+    });
+  });
+});
