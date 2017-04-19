62a5c2cce68c1631e624f54a446fe2d938360dc3
KaiCYu <kaichyu@gmail.com>
2017-04-19 11:14:10 -0700

    working refactor of signup employer promise chain

--- server/promises.js
+++ server/promises.js
@@ -1,21 +1,15 @@
 const db = require('./db/index.js').connection;
 const Promise = require("bluebird");
 const cloudinary = require('cloudinary');
-
+// const cloudinary = Promise.promisify(require('cloudinary'));
 // db = Promise.promisifyAll(db, { multiArgs: true });
 
 const checkUsername = (queryStr) => {
-  console.log('QUERY STRING: ', queryStr);
   return new Promise((resolve, reject) => {
-    console.log('inside promise.js')
     db.queryAsync(queryStr).then((data) => {
-      console.log('inside db.queryAsync');
-      console.log('DATA', data);
-      if (data.length !== 0) {
         const redirectUrl = `import React from 'react';
           const redirect = () => (
           <Redirect to="${req._parsedOriginalUrl.path}">);`;
-          // console.log('REDIRECT ', redirectUrl);
         res.send(redirectUrl);
       } else {
         resolve();
@@ -26,24 +20,18 @@ const checkUsername = (queryStr) => {
 
 const uploadToCloudinaryAsync = (file) => {
   return new Promise((resolve, reject) => {
-    cloudinary.v2.uploader.upload(file, {resource_type: 'auto'}, (result) => {
-      resolve(result);
-    })
-  })
-}
-
-// const insertEmployer = `INSERT INTO employer (username, password, company_name, email, phone_number, city, state, country, logo_url) values
-//   ("${req.body.username}", "${hash}", "${req.body.companyName}",
-//   "${req.body.email}", "${req.body.phoneNumber}", "${req.body.city}", "${req.body.state}", "${req.body.country}", "${image.secure_url}"
-// );`;
-
+      if (result) {
+        resolve(result);
+        console.log('error');
+        reject(error);
+    });
+  });
+};
 const insertEmployerToDB = (queryStr) => {
   return new Promise((resolve, reject) => {
     db.queryAsync(queryStr);
   })
 }
-
-
 
 module.exports = {
   insertEmployerToDB,
